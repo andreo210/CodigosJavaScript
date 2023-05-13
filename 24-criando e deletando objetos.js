474d57b8f256b48d4ff4517f627c499274c041db
Andre <andreo210@hotmail.com>
2023-05-13 17:03:16 -0300

    vovos codigos

---
+++ 24-criando e deletando objetos.js
@@ -0,0 +1,11 @@
+let pessoa = {
+    NOME: "Mateus",
+    profissão: "Programador",
+    idade: "39"
+}
+
+delete pessoa.NOME;//deletando
+console.log(pessoa)
+
+pessoa.casado = true;//criando
+console.log(pessoa)
---
+++ 27-mutacao.js
@@ -0,0 +1,9 @@
+let pessoa = {
+    NOME: "Mateus",
+    profissão: "Programador",
+    idade: "39"
+}
+
+let pessoa2 = pessoa;
+
+console.log(pessoa == pessoa2)
