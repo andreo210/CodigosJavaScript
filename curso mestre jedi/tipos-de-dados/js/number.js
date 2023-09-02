74027bd21ce369c6f33a43fe30ce834009683913
Andre <andreo210@hotmail.com>
2023-09-02 19:33:26 -0300

    number jedi

--- curso mestre jedi/tipos-de-dados/js/number.js
+++ curso mestre jedi/tipos-de-dados/js/number.js
@@ -1,12 +1,12 @@
 var idade = 28;
 console.log(idade);
 
-console.log(typeof idade);
+console.log(typeof idade);//verifica o tipo
 
 var precoFeijao = 2.89;
 
 console.log(precoFeijao);
-console.log(typeof precoFeijao);
+console.log(typeof precoFeijao);//verifica o tipo
 
 var valor = 23 + 33;
 console.log(valor);
@@ -17,11 +17,11 @@ console.log(valor);
 console.log(numero);
 console.log(numero + valor);
 
-console.log(Number.MAX_VALUE);
-console.log(Number.MIN_VALUE);
+console.log(Number.MAX_VALUE);//numero maximo que o javascript suporta
+console.log(Number.MIN_VALUE);//numero minimo que o javascript suporta
 
 console.log(valor);
-console.log(valor.toString(10));
-console.log(valor.toString(2));
-console.log(valor.toString(8));
-console.log(valor.toString(16));
+console.log(valor.toString(10));//imprime o valor na base 10
+console.log(valor.toString(2));//imprime o valor na base 2
+console.log(valor.toString(8));//imprime o valor na base 8
+console.log(valor.toString(16));//imprime o valor na base 16
