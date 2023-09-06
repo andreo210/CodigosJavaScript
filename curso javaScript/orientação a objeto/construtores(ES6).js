6248046eb1d3bd8fdfe717bc769c9009da1c312b
Andre <andreo210@hotmail.com>
2023-09-06 20:03:12 -0300

    novos codigos

---
+++ curso javaScript/orientação a objeto/construtores(ES6).js
@@ -0,0 +1,9 @@
+class Cachorro{
+    constructor(raca,pata,cor){
+        this.raca = raca;
+        this.cor = cor;
+        this.pata =pata;
+    }
+}
+let labrador = new Cachorro("labrador", 4, "preto")
+console.log(labrador)
