14f2b4fd5648c29cf2b2eff893b5b43f75406289
andreo210 <98775473+andreo210@users.noreply.github.com>
2023-09-13 08:27:43 -0300

    declaracoes js

---
+++ curso java script w3School/Declaracoes JS/declaracoes.js
@@ -0,0 +1,16 @@
+let x, y, z;    // Statement 1
+x = 5;          // Statement 2
+y = 6;          // Statement 3
+z = x + y;      // Statement 4
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+console.log("---------------------------------------------------------------------------------------------------------------------------------------------------");
+
+//Quando separados por ponto e vírgula, são permitidas múltiplas instruções em uma linha:
+a = 5; b = 6; c = a + b;
+console.log(a);
+console.log(b);
+console.log(c);
