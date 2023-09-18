63f4f10746d5228ea1c0c72e82939524fb6723e5
andreo210 <98775473+andreo210@users.noreply.github.com>
2023-09-18 08:49:56 -0300

    foreach

---
+++ curso java script w3School/Sintaxe.js
@@ -0,0 +1,15 @@
+// How to create variables:
+var x;
+let y;
+
+// How to use variables:
+x = 5;
+y = 6;
+let z = x + y;
+
+/*
+Operadores JavaScript
+JavaScript usa operadores aritméticos ( + - * /) para calcular valores
+*/
+(5 + 6) * 10
+
